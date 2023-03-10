cfc41bf8742b7fe7f62ff428aca8eb71f0dae9a8
AjnaKaric <ajnakaric296@gmail.com>
2023-03-10 20:59:39 +0100

    I added two more questions to the quiz and modified existing questions.

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ const questions = [
     b: "Coding Style Sheets",
     c: "Cascading Simple Sheets",
     d: "Cars SUVs & Sailboats",
+    e: "Cascading Style Sheet",
     correct: "a",
   },
   {
@@ -14,6 +15,7 @@ const questions = [
     b: "2021",
     c: "2022",
     d: "2023",
+    e: "2019",
     correct: "b",
   },
   {
@@ -21,7 +23,8 @@ const questions = [
     a: "Bosnia and Herzegovina Famous Foundation",
     b: "Bosnia and Herzegovina Futures Foundation",
     c: "Bosnia and Herzegovina Factory Foundation",
-    d: "none of the above",
+    d: "Bosnia and Herzegovina Fun Foundation",
+    e: "none of the above",
     correct: "b",
   },
   {
@@ -30,8 +33,19 @@ const questions = [
   b: "30",
   c: "15",
   d: "16",
+  e: "51",
   correct: "c",
-  }
+  },
+ {
+    question: "What command do we use to initalize a local Git repositroy?",
+    a: "git init",
+    b: "git commit",
+    c: "cd repo_name",
+    d: "git add",
+    e: "git status",
+    correct: "a",
+   },
+ 
 ];
 
 //Fetching the HTML elements
@@ -42,6 +56,8 @@ const a_text = document.getElementById("a_text");
 const b_text = document.getElementById("b_text");
 const c_text = document.getElementById("c_text");
 const d_text = document.getElementById("d_text");
+const e_text = document.getElementById("e_text");
+
 const submitButton = document.getElementById("submit");
 
 //Counters
@@ -71,6 +87,7 @@ const loadQuiz = () => {
   b_text.innerText = currentQuizData.b;
   c_text.innerText = currentQuizData.c;
   d_text.innerText = currentQuizData.d;
+  e_text.innerText = currentQuizData.e;
 };
 
 //Loading the quiz
